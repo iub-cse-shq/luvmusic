54b98c5321bc9460c1865c4f6dc29465b7924fcf
Sajid Zaman <zsajid014@gmail.com>
2018-09-02 17:55:17 +0000

    Initial commit

--- luvmusic-song-recommendation/Clustering.js
+++ luvmusic-song-recommendation/Clustering.js
@@ -1,51 +1,46 @@
-var np = require('numpy-js');
-var sqrt = require('math-js');
-var plt = require('plotly');
 var array = require('numpy-js');
-var pd = require('pandas-js');
+var plt = require('plotly-js');
+var array = require('numpy-js');
 var kMeans = require('kmeans-js');
+var kMeansModel = require('kmeans-js');
 
 //Load and parse the data
-var data = pd.readCSV('datasets.csv');
+var data = pd.loadCSV('datasets.csv');
 data.take(2);
 
 //Make a list from every line
-var data = data.map(function(x) {return x.split('\t')});
-var header = data.first();
+var data = data.map(function(x) {
+   return x.split('\t');
+});
 
-//Clean data
-var data2 = data.filter(function(line) {return line!=header});
-console.log("Length of uncleaned data: " + toString(data2.count()));
-data2.first();
+//Cleaning data
+var data2 = data.filter(function(line){
+    return line !== header;
+});
+
+console.log(data2.count());
 
 //Filter the data
-var data3 = data2.filter(function(x) {return x[4].length === 1 && x[5].length > 1});
+var data3 = data2.filter(function(x){
+    return x[1].length === 1 && x[2] > 1;
+});
+
 data3.count();
 
 //Encode the data
-var title = ["Sawaali si raat", "Cholo Brishtite Bhiji", "Take a lool around"];
-var artist = ["Arijit Singh", "Habib and Sabina Yasmin", "Limp Bizkit"];
+var user = {'male': 0, 'female': 1};
 
-function mapr1(key) {
-    return title[key];
+//Define mapper function
+function mapper(key) {
+    return user[key];
 }
 
-function mapr2(key) {
-    return artist[key];
+var data4 = data3.map(function(x){
+    return mapper[x]
+});
+
+//Cluster the data and calculating the mean squared error
+function error (x) {
+    var clusters, centers;
+    var center = clusters.centers();
 }
-
-var data4 = data3.map(function(x){return [mapr1(x[4]), float(x[5]), mapr2(x[6])]});
-data4.count();
-
-//Clustering the data
-function error(point) {
-    var center = clusters.centers[clusters.predict(point)];
-    var centroid = point - center;
-    var total = 0;
-    for (var i = 0; i < centroid; i++) {
-        total += Math.pow(i, 2);
-    }
-    return total;
-}
-
-var 
